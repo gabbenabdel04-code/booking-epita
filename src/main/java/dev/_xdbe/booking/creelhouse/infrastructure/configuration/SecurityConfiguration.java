96c04f28155cc1be327cd2790bdf1ee9a2f08e58
gabriel.ben-abdeljelil <gabriel.ben-abdeljelil@epita.fr>
2026-04-15 11:52:26 +0200

    feat: add login form

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -28,11 +28,13 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(auth -> auth
                 // Step 4a: add access control
                 // ...
+                .requestMatchers("/dashboard").hasRole("ADMIN")
                 // Step 4a: end
                 .anyRequest().permitAll()
             )
             // Step 4b: Add login form
             // ...
+            .formLogin(withDefaults())
             // Step 4b: End of login form configuration
             
             .csrf((csrf) -> csrf
