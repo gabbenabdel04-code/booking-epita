961b9330f7e05caf849d5a92063eadefae30c377
gabriel.ben-abdeljelil <gabriel.ben-abdeljelil@epita.fr>
2026-04-15 12:07:30 +0200

    feat: credit card number masking

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
@@ -38,7 +38,13 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
 
     private String panMasking(String pan) {
         // Step 6:
-        return pan;
+        if (pan == null || pan.length() <= 8) {
+            return pan;
+        }
+        String first4 = pan.substring(0, 4);
+        String last4 = pan.substring(pan.length() - 4);
+        String masked = "*".repeat(pan.length() - 8);
+        return first4 + masked + last4;
         // Step 6: End
     }
 
